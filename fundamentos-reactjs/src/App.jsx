eee269b3d438154f8cda447bba1cc3c6a7f1b290
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-08-06 22:40:38 -0400

    Entendendo css module

---
+++ fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,18 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <div>
+      
+    <Header />
+    
+      <Post
+        author="Deyvson Aguiar"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet maxime rem molestiae quisquam quaerat, delectus aliquid impedit laborum. Sunt dignissimos saepe eum laboriosam doloremque fuga voluptatibus dicta vitae est quisquam." 
+        />
+      
+      </div>
+  )
+}
+
