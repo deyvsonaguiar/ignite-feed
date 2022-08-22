82c23ae4c6acb378511e1eec2878f3aa1e733b84
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-08-21 22:29:36 -0400

    Componente Post

--- fundamentos-reactjs/src/App.jsx
+++ fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,8 @@
 import { Header } from "./components/Header";
-import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 import styles from "./App.module.css";
-import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
@@ -13,14 +13,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-          author="Deyvson Aguiar"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet maxime rem molestiae quisquam quaerat, delectus aliquid impedit laborum. Sunt dignissimos saepe eum laboriosam doloremque fuga voluptatibus dicta vitae est quisquam." 
-          />
-          <Post
-          author="Ruth Almeida"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet maxime rem molestiae quisquam quaerat, delectus aliquid impedit laborum. Sunt dignissimos saepe eum laboriosam doloremque." 
-          />
+          <Post/>
+          <Post/>
         </main>
       </div>
       
